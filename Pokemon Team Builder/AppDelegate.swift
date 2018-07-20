88034dd618222a3153dc8293f93b4a4b2f91ce28
Lygre <hughdbroome@gmail.com>
2018-07-20 13:54:13 -0400

    Initial Commit

---
+++ Pokemon Team Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pokemon Team Builder
+//
+//  Created by Hugh Broome on 7/20/18.
+//  Copyright © 2018 Hugh Broome. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Pokemon Team BuilderTests/Pokemon_Team_BuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pokemon_Team_BuilderTests.swift
+//  Pokemon Team BuilderTests
+//
+//  Created by Hugh Broome on 7/20/18.
+//  Copyright © 2018 Hugh Broome. All rights reserved.
+//
+
+import XCTest
+@testable import Pokemon_Team_Builder
+
+class Pokemon_Team_BuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
