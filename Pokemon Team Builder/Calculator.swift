c135ae051db49cd950fabc37aa80fa6a49bc908c
Hugh Broome <hughdylanbroome@gmail.com>
2018-09-20 11:57:54 -0400

    Working in Calculator.swift to create damage calculation methods

--- Pokemon Team Builder/Calculator.swift
+++ Pokemon Team Builder/Calculator.swift
@@ -31,9 +31,75 @@ class Field: NSObject {
 }
 
 func calculateAllMoves(p1: Pokemon, p2: Pokemon, field: Field) {
+	//checkStatBoost(p1, p2)
+	//var results = getDamageResult()
 	
 }
 
+func getDamageResult(attacker: Pokemon, defender: Pokemon, move: Move, field: Field) -> [Move: Int] {
+	var dmgResult: [Move: Int] = [Move: Int]()
+	var baseDamage: Int = Int()
+	var finalD = Int()
+	var D: Int = Int()
+	var attack = Int()
+	var defense = Int()
+	var modifier = Double()
+	
+	if move.category == "Physical" {
+//		D = (((((2*attacker.level/5)+2) * move.basePower * (attacker.virtualStats["atk"] / defender.virtualStats["def"])) / 50)+2)
+	} else if move.category == "Special" {
+//		D = (((((2*attacker.level/5)+2) * move.basePower * (attacker.virtualStats["spa"] / defender.virtualStats["spd"])) / 50)+2)
+	}
+	var weather, crit, random, stab, type, burn, other: Double
+	//calc weather mod
+	if field.weather != "None" {
+		if field.weather == "Rain" && move.type == "Water" {
+			weather = 1.5
+		} else if field.weather == "Rain" && move.type == "Fire" {
+			weather = 0.5
+		} else if field.weather == "Sun" && move.type == "Water" {
+			weather = 0.5
+		} else if field.weather == "Sun" && move.type == "Fire" {
+			weather = 1.5
+		}
+	} else { weather = 1.0 }
+	// crit mod to fix later
+	crit = 1.0
+	// random to make later
+	random = 1.0
+	//stab mod
+	for type in attacker.types {
+		if move.type == type {
+			stab = 1.5
+			break
+		}
+	}
+	//type mod
+	var defenderTypeTable: [String: Int] = defender.getPokemonWeaknesses(pokemonName: defender)
+	//make iterator over dictionary here
+	modifier = 1.0
+	
+	var modD = Double.init(D) * modifier
+	finalD = Int.init(modD)
+	
+	
+	
+	return dmgResult
+}
+
+//func getFinalDamage(level: Int, modifier: Double, power: Int, d:, e: ) -> Int {
+//	var finalD = Int()
+//	var D = (((((2*level/5)+2) * power * (attack / defense)) / 50)+2)
+//	var modD = Double.init(D) * modifier
+//	finalD = Int.init(modD)
+//
+//
+//
+//	return finalD
+//}
+
+
+//----------------------------- Class-based resist search functions
 func resistSearch(types: [String]) -> [Pokemon] {
 	var matchMons: [Pokemon] = [Pokemon]()
 	
